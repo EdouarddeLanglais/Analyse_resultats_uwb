f133bc64905545b4ee050ce122882be97f0f6e56
EdouarddeLanglais <98285731+EdouarddeLanglais@users.noreply.github.com>
2025-08-28 13:47:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ time.sleep(0.1)
 
 # Setup
 if mode == "receiver":
-    calibration_string1 = "CALKEY ant0.ch5.ant_delay " + settings['receiver_calibration']['ch5_antenna_delay'] + "\n"
-    calibration_string2 = "CALKEY ant0.ch9.ant_delay " + settings['receiver_calibration']['ch9_antenna_delay'] + "\n"
+    calibration_string1 = "CALKEY ant0.ch5.ant_delay " + settings['receiver_calibration']['ch5_antenna_delay'] + "\r\n"
+    calibration_string2 = "CALKEY ant0.ch9.ant_delay " + settings['receiver_calibration']['ch9_antenna_delay'] + "\r\n"
     com_mode = "RESPF"
 
 elif mode == "initiator":
-    calibration_string1 = "CALKEY ant0.ch5.ant_delay " + settings['initiator_calibration']['ch5_antenna_delay'] + "\n"
-    calibration_string2 = "CALKEY ant0.ch9.ant_delay " + settings['initiator_calibration']['ch9_antenna_delay'] + "\n"
+    calibration_string1 = "CALKEY ant0.ch5.ant_delay " + settings['initiator_calibration']['ch5_antenna_delay'] + "\r\n"
+    calibration_string2 = "CALKEY ant0.ch9.ant_delay " + settings['initiator_calibration']['ch9_antenna_delay'] + "\r\n"
     com_mode = "INITF"
 
 else:
@@ -55,22 +55,16 @@ time.sleep(0.1)
 
 com_settings = settings['com_settings']
 
-com_channel = "CHAN="+str(com_settings['channel'])              # UWB channel
-com_prf = "PRFSET="+com_settings['prf']                         # Pulse Repetition Frequency set
-com_pcode = "PCODE="+str(com_settings['preamble_code'])         # Preable Code Index
-com_slot = "SLOT="+str(com_settings['ranging_slot_duration'])   # Ranging slot duration in RSTU, 1ms = 1200 RSTU
-com_block = "BLOCK="+str(com_settings['ranging_block'])         # Ranging block duration
-com_round = "ROUND="+str(com_settings['round_duration'])        # Number of ranging operation in one slot
-com_ranging_type = "RRU="+com_settings['ranging_type']          # Ranging type preferred
+com_channel = "-CHAN="+str(com_settings['channel'])              # UWB channel
+com_prf = "-PRFSET="+com_settings['prf']                         # Pulse Repetition Frequency set
+com_pcode = "-PCODE="+str(com_settings['preamble_code'])         # Preable Code Index
+com_slot = "-SLOT="+str(com_settings['ranging_slot_duration'])   # Ranging slot duration in RSTU, 1ms = 1200 RSTU
+com_block = "-BLOCK="+str(com_settings['ranging_block'])         # Ranging block duration
+com_round = "-ROUND="+str(com_settings['round_duration'])        # Number of ranging operation in one slot
+com_ranging_type = "-RRU="+com_settings['ranging_type']          # Ranging type preferred
 
-com_line = com_mode+" "+com_channel+" "+com_prf+" "+com_pcode+" "+com_slot+" "+com_block+" "+com_round+" "+com_ranging_type+"\n"
+com_line = com_mode+" "+com_channel+" "+com_prf+" "+com_pcode+" "+com_slot+" "+com_block+" "+com_round+" "+com_ranging_type+"\r\n"
 
-ser.write(com_line.encode('ascii'))
-time.sleep(0.1)
-ser.write(b"STOP\n")
-time.sleep(0.1)
-ser.write(b"SAVE\n")
-time.sleep(0.1)
 ser.write(com_line.encode('ascii'))
 
 start_measuring = time.perf_counter()
@@ -96,7 +90,7 @@ while timestamp < measurement_time:
                 results.append(get_number_from_string(line, start, end, int))
                 timestamps.append(timestamp)
 
-ser.write(b"STOP\n")
+ser.write(b"STOP\r\n")
 time.sleep(0.1)
 ser.close()
 
